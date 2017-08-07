d62b6228c792900db217f223e10e420efb2b0d84
25030043 <30688665+25030043@users.noreply.github.com>
2017-08-07 13:19:48 +0200

    Add files via upload

---
+++ timothy barnardo/max/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using ITRW324___Github_Assignment___Group_17;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("My name is Timohty Barnardo \nI study Bsc. IT \nI am currently a 4th year at the NWU Pukke campus \nEmail address: barnardotimothy@gmail.com");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] arrNumbers = { 22, 15, 30, 60, 87, 779, 11 };
+            label1.Text = "22, 15, 30, 60, 87, 779, 11";
+            MessageBox.Show(arrNumbers.Max().ToString());
+            //int txt1 = Convert.ToInt32(textBox1.Text);
+            //Random rand = new Random();
+
+            //int[] rn = new int[txt1];
+            //int tot = 0;
+            //string arr = "";
+
+            //for (int i = 0; i < txt1; i++)
+            //{
+            //    int num = rand.Next(0, 100);
+            //    rn[i] = num;
+            //    tot += num;
+            //    arr += Convert.ToString(num) + " ";
+            //}
+            //label1.Text = arr;
+
+            //int max = arr[0];
+            //for (int k = 1; k < arr.Length; k++)
+            //{                               
+
+            //    if (max < arr[k]) max = arr[k];
+
+            //}
+            //MessageBox.Show("max value is" + max);
+
+
+
+
+        }
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MedianFunctions med = new MedianFunctions();
+            int[] arrNumbers = { 22, 15, 30, 60, 87, 779, 11 };
+            med.sortArray(arrNumbers);
+            
+            label3.Text = med.calcMedian(arrNumbers).ToString();
+
+        }
+    }
+}
---
+++ timothy barnardo/max/WindowsFormsApplication2/MedianFunctions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITRW324___Github_Assignment___Group_17
+{
+    class MedianFunctions
+    {
+        public int[] sortArray(int[] arrReceived)
+        {
+            Array.Sort<int>(arrReceived);
+
+            //int arrlenght = arrreceived.length;
+            //int temp = 0;
+            ////actual sorting
+            //for (int i = 0; i < arrreceived.length; i++)
+            //{
+            //    for (int j = 0; j < arrreceived.length - 1; j++)
+            //    {
+            //        if (arrreceived[j] > arrreceived[j + 1])
+            //        {
+            //            temp = arrreceived[j + 1];
+            //            arrreceived[j + 1] = arrreceived[j];
+            //            arrreceived[j] = temp;
+            //        }//if end
+            //    }//j for end
+
+            //}//i for end
+            return arrReceived;
+        }//sortArray end   
+
+        public int calcMedian(int[] SortedArray)
+        {
+            int median = 0;
+            if (SortedArray.Length % 2 == 0)
+            {
+                int element1 = SortedArray[(SortedArray.Length / 2) - 1];
+                int element2 = SortedArray[(SortedArray.Length / 2)];
+                    median = (element1 + element2) / 2;
+                }
+                else
+                    median = SortedArray[(SortedArray.Length / 2)];
+
+            
+            return median;
+            }
+        }//class end
+    }//namespace end
