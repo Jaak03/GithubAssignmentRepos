416103b3e5132e64b12562eb20a6b9f1e4e23c55
leandridutoit <30688804+leandridutoit@users.noreply.github.com>
2017-08-07 13:09:30 +0200

    Add files via upload

---
+++ TestNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    class TestNumber
+    {
+
+        public TestNumber()
+        {
+        }
+
+        public bool test(double number)
+        {
+            bool isValid = true;
+
+            if(number < 5 || number > 20)
+            {
+                isValid = false;
+            }
+
+            return isValid;
+        }
+    }
+}
